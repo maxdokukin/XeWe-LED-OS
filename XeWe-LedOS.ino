1ebc01f0886b8993e364a0ac5da4eca09e25827b
maxdokukin <tools.shiny.01@icloud.com>
2025-05-30 21:34:35 -0700

    okay created config.h and it works

--- XeWe-LedOS.ino
+++ XeWe-LedOS.ino
@@ -1,20 +1,16 @@
 #include <FastLED.h>
+#include "src/Config.h"
 #include "src/SystemController/SystemController.h"
 
-#define LED_PIN     0
-#define LED_TYPE    WS2815
-// #define COLOR_ORDER GRB
-#define COLOR_ORDER BRG
-
-#define NUM_LEDS_MAX    1000
-#define BRIGHTNESS_MAX  255
-CRGB leds[NUM_LEDS_MAX];
+CRGB leds[LED_STRIP_NUM_LEDS_MAX];
+// CRGB system_led_indicator[1];
 
 SystemController * led_os = nullptr;
 
 void setup() {
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS_MAX).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS_MAX );
+    FastLED.addLeds<LED_STRIP_TYPE, PIN_LED_STRIP, LED_STRIP_COLOR_ORDER>(leds, LED_STRIP_NUM_LEDS_MAX).setCorrection( TypicalLEDStrip );
+//     FastLED.addLeds<LED_INDICATOR_TYPE, PIN_LED_INDICATOR, LED_INDICATOR_COLOR_ORDER>(system_led_indicator, 1).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(255);
 
     led_os = new SystemController(leds);
 }
