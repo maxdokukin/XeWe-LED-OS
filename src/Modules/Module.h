3af68b406290f1d5100f3570a4f9b48b4d2d703e
maxdokukin <tools.shiny.01@icloud.com>
2025-07-30 14:58:28 -0700

    new module logic

--- src/Modules/Module.h
+++ src/Modules/Module.h
@@ -1,62 +1,124 @@
-// src/Modules/Module.h
 #ifndef MODULE_H
 #define MODULE_H
 
-#include <cstdint>
-#include <type_traits>
-class SystemController;
-#include "Software/CommandParser/CommandParser.h"
+#include <cstddef>      // for std::size_t
+#include <functional>   // for std::function
+#include <string>       // for std::string
+#include <utility>      // for std::move
 
-template<typename ContextT = void>
+class SystemController;
+
+class ModuleConfig {
+public:
+    ModuleConfig() {}
+
+    virtual ~ModuleConfig() noexcept = default;
+
+    ModuleConfig(const ModuleConfig&)            = default;
+    ModuleConfig& operator=(const ModuleConfig&) = default;
+    ModuleConfig(ModuleConfig&&) noexcept         = default;
+    ModuleConfig& operator=(ModuleConfig&&) noexcept = default;
+};
+
+using command_function_t = std::function<void(const char* args)>;
+
+struct Command {
+    std::string         name;
+    std::string         description;
+    std::string         sample_usage;
+    std::size_t         arg_count;
+    command_function_t  function;
+};
+
+struct CommandGroup {
+    std::string    name;
+    const Command* commands;
+    std::size_t    command_count;
+};
+
+/// Generic Module base: parameterized only by your runtime‐context type.
+template<typename ContextT>
 class Module {
 public:
-    using Command      = CommandParser::Command;
-    using CommandGroup = CommandParser::CommandGroup;
-
-    /**
-     * @tparam N
-     *   Deduce the number of commands in your static Command array.
-     *   We `static_assert(N>0)` to enforce at least 1 command at compile time.
-     */
-    template<size_t N>
-    Module(SystemController& controller_ref,
-           const char*       module_name_param,
-           const Command     (&cmds)[N],
-           bool              initially_enabled,
-           bool              can_be_disabled,
-           const char*       nvs_key_param)
+    Module(SystemController&  controller_ref,
+           std::string        module_name_param,
+           std::string        nvs_key_param,
+           bool               can_be_disabled)
       : controller(controller_ref)
-      , module_name(module_name_param)
-      , enabled(initially_enabled)
+      , module_name(std::move(module_name_param))
+      , nvs_key(std::move(nvs_key_param))
+      , enabled(true)
       , can_be_disabled(can_be_disabled)
-      , nvs_key(nvs_key_param)
-      , commands(cmds)
-      , cmd_count(N)
-      , commands_group{ module_name_param, commands, cmd_count }
-    {
-        static_assert(N > 0, "Every Module must define at least one Command");
-    }
+    {}
 
     virtual ~Module() noexcept = default;
 
-    virtual void begin(ContextT* context = nullptr) = 0;
-    virtual void loop(ContextT* context = nullptr)  = 0;
-    virtual void enable()                          = 0;
-    virtual void disable()                         = 0;
-    virtual void reset()                           = 0;
-    virtual const char* status()                   = 0;
+    // no copy/move
+    Module(const Module&)            = delete;
+    Module& operator=(const Module&) = delete;
+    Module(Module&&)                 = delete;
+    Module& operator=(Module&&)      = delete;
+
+    // **begin takes ModuleConfig, loop takes only ContextT**
+    virtual void begin(const ModuleConfig& cfg) = 0;
+    virtual void loop(ContextT ctx)             = 0;
+
+    virtual void enable()                = 0;
+    virtual void disable()               = 0;
+    virtual void reset()                 = 0;
+    virtual const char* status() const   = 0;
 
 protected:
     SystemController&   controller;
-    const char*         module_name;
-    const char*         nvs_key;
+    std::string         module_name;
+    std::string         nvs_key;
     bool                enabled;
     bool                can_be_disabled;
 
-    // owned by base now:
-    const Command*      commands;
-    size_t              cmd_count;
-    CommandGroup        commands_group;
+    const Command*      commands       = nullptr;
+    std::size_t         cmd_count      = 0;
+    CommandGroup        commands_group{};
+};
+
+// Specialization for no‐context modules: begin()/loop() take no args.
+template<>
+class Module<void> {
+public:
+    Module(SystemController&  controller_ref,
+           std::string        module_name_param,
+           std::string        nvs_key_param,
+           bool               can_be_disabled)
+      : controller(controller_ref)
+      , module_name(std::move(module_name_param))
+      , nvs_key(std::move(nvs_key_param))
+      , enabled(true)
+      , can_be_disabled(can_be_disabled)
+    {}
+
+    virtual ~Module() noexcept = default;
+
+    Module(const Module&)            = delete;
+    Module& operator=(const Module&) = delete;
+    Module(Module&&)                 = delete;
+    Module& operator=(Module&&)      = delete;
+
+    virtual void begin()              = 0;
+    virtual void loop()               = 0;
+    virtual void enable()             = 0;
+    virtual void disable()            = 0;
+    virtual void reset()              = 0;
+    virtual const char* status() const= 0;
+
+protected:
+    SystemController&   controller;
+    std::string         module_name;
+    std::string         nvs_key;
+    bool                enabled;
+    bool                can_be_disabled;
+
+    const Command*      commands       = nullptr;
+    std::size_t         cmd_count      = 0;
+    CommandGroup        commands_group{};
 };
 
 #endif // MODULE_H
