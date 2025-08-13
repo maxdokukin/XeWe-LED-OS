294557749fd37ade12389fcae456e79db528905b
maxdokukin <tools.shiny.01@icloud.com>
2025-08-13 14:23:58 -0700

    upd serialport

--- src/Modules/Software/SerialPort/SerialPort.h
+++ src/Modules/Software/SerialPort/SerialPort.h
@@ -1,45 +1,47 @@
 // src/Modules/Software/SerialPort/SerialPort.h
 #pragma once
 
+#include "../../Module/Module.h"
+#include "../../../Debug.h"
 #include <functional>
 #include <string>
 #include <string_view>
-#include <cstdlib>    // for std::strtol
+#include <cstdlib>
 #include <cstring>
 
-#include "../../Module/Module.h"
-#include "../../../Debug.h"
-
 struct SerialPortConfig : public ModuleConfig {
-    unsigned long baud_rate = 115200;
+    unsigned long               baud_rate                   = 115200;
 };
+
 
 class SerialPort : public Module {
 public:
-    explicit                    SerialPort          (SystemController& controller);
+    explicit                    SerialPort                  (SystemController& controller);
 
-    void                        begin               (const ModuleConfig& cfg)               override;
-    void                        loop                ()                                      override;
-    void                        reset               (bool verbose=false)                    override;
+    // required implementation
+    void                        begin                       (const ModuleConfig& cfg)       override;
+    void                        loop                        ()                              override;
+    void                        reset                       (bool verbose=false)            override;
 
-    // Extra API (String‐based)
-    void                        print               (std::string_view message);
-    void                        println             (std::string_view message);
-    bool                        has_line            ()                                      const;
+    // other methods
+    void                        print                       (std::string_view message);
+    void                        println                     (std::string_view message);
 
-    std::string                 read_line           ();
-    std::string                 get_string          (std::string_view prompt = {});
-    int                         get_int             (std::string_view prompt = {});
-    bool                        get_confirmation    (std::string_view prompt = {});
-    bool                        prompt_user_yn      (std::string_view prompt = {}, uint16_t timeout = 10000);
-    void                        print_spacer        ();
+    bool                        has_line                    () const;
+    std::string                 read_line                   ();
+
+    std::string                 get_string                  (std::string_view prompt = {});
+    int                         get_int                     (std::string_view prompt = {});
+    bool                        get_confirmation            (std::string_view prompt = {});
+    bool                        prompt_user_yn              (std::string_view prompt = {},                                                              uint16_t timeout = 10000);
+    void                        print_spacer                ();
 
 private:
-    size_t                     input_buffer_pos = 0;
-    size_t                     line_length      = 0;
-    bool                       line_ready       = false;
-    static constexpr size_t    INPUT_BUFFER_SIZE = 256;
-    char                       input_buffer[INPUT_BUFFER_SIZE];
+    size_t                     input_buffer_pos             = 0;
+    size_t                     line_length                  = 0;
+    bool                       line_ready                   = false;
+    static constexpr size_t    INPUT_BUFFER_SIZE            = 256;
+    char                       input_buffer                 [INPUT_BUFFER_SIZE];
 
-    void                       flush_input        ();
+    void                       flush_input                  ();
 };
