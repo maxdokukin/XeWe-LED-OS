4dd5ccc2ae128cb95301c36bb53d6a5ddab10f51
maxdokukin <tools.shiny.01@icloud.com>
2025-08-13 14:24:23 -0700

    upd wifi

--- src/Modules/Software/Wifi/Wifi.h
+++ src/Modules/Software/Wifi/Wifi.h
@@ -1,9 +1,8 @@
-#ifndef WIFI_H
-#define WIFI_H
+// src/Modules/Wifi/Wifi.h
+#pragma once
 
 #include "../../Module/Module.h"
 #include "../../../Debug.h"
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <vector>
@@ -15,38 +14,39 @@ struct WifiConfig : public ModuleConfig {
     std::string hostname = "ESP32-C3-Device";
 };
 
+
 class Wifi : public Module {
 public:
-    explicit Wifi(SystemController& controller);
+    explicit                    Wifi                        (SystemController& controller);
 
-    void                begin               (const ModuleConfig& cfg) override;
-    void                loop                ()                          override;
-    void                reset               (bool verbose=false)        override;
+    // required implementation
+    void                        begin                       (const ModuleConfig& cfg)       override;
+    void                        loop                        ()                              override;
+    void                        reset                       (bool verbose=false)            override;
 
-    bool                enable              (bool verbose=false)        override;
-    bool                disable             (bool verbose=false)        override;
-    std::string         status              (bool verbose=false) const  override;
+    // other methods
+    bool                        enable                      (bool verbose=false)            override;
+    bool                        disable                     (bool verbose=false)            override;
+    std::string                 status                      (bool verbose=false) const      override;
 
-    bool                connect             (bool prompt_for_credentials);
-    bool                disconnect          (bool verbose=false);
-    bool                is_connected        (bool verbose=false) const;
-    bool                is_disconnected     (bool verbose=false) const;
+    bool                        connect                     (bool prompt_for_credentials);
+    bool                        disconnect                  (bool verbose=false);
+    bool                        is_connected                (bool verbose=false) const;
+    bool                        is_disconnected             (bool verbose=false) const;
 
-    std::string         get_local_ip        ()                          const;
-    std::string         get_ssid            ()                          const;
-    std::string         get_mac_address     ()                          const;
+    std::string                 get_local_ip                () const;
+    std::string                 get_ssid                    () const;
+    std::string                 get_mac_address             () const;
 
 private:
-    std::vector<std::string> scan(bool verbose);
+    std::vector<std::string>    scan                        (bool verbose);
 
-    bool   join                        (std::string_view ssid,
-                                       std::string_view password,
-                                       uint16_t timeout_ms=10000,
-                                       uint8_t retry_count=1);
-    bool   read_stored_credentials     (std::string& ssid,
-                                       std::string& password);
-    uint8_t prompt_credentials          (std::string& ssid,
-                                       std::string& password);
+    bool                        join                        (std::string_view ssid,
+                                                             std::string_view password,
+                                                             uint16_t timeout_ms=10000,
+                                                             uint8_t retry_count=1);
+    bool                        read_stored_credentials     (std::string& ssid,
+                                                             std::string& password);
+    uint8_t                     prompt_credentials          (std::string& ssid,
+                                                             std::string& password);
 };
-
-#endif // WIFI_H
