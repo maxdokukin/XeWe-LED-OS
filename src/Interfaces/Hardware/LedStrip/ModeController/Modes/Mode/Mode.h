ebc7c409fcc3b7a46fe7e1c3b6ee62777cf3b848
Max <tools.shiny.01@icloud.com>
2026-03-05 09:54:26 -0800

    polishing

--- src/Interfaces/Hardware/LedStrip/ModeController/Modes/Mode/Mode.h
+++ src/Interfaces/Hardware/LedStrip/ModeController/Modes/Mode/Mode.h
@@ -19,11 +19,21 @@ struct ModeConfig {
     uint8_t id;
     std::string name;
     std::vector<ModeParam> params;
+
+    ModeConfig(uint8_t init_id, std::string init_name, const std::vector<ModeParam>& custom_params = {})
+        : id(init_id), name(init_name)
+    {
+        params.push_back({"r", "R",   0, 255, 255, 1});
+        params.push_back({"g", "G", 0, 255, 255, 1});
+        params.push_back({"b", "B",  0, 255, 255, 1});
+        params.insert(params.end(), custom_params.begin(), custom_params.end());
+    }
 };
 
 class Mode {
 public:
     explicit Mode(const ModeConfig& mode_config) : config(mode_config) {}
+    virtual ~Mode() = default;
 
     virtual void loop(CRGB* leds, uint16_t num_leds) = 0;
 
