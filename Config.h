6bc2cafed87f3f07cb1328f5ef0d2ca39a12bd5c
Max <tools.shiny.01@icloud.com>
2026-03-08 23:39:01 -0700

    migrating to new release format

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@
 // and as calculated as max_fps = 1000000 / (LED_STRIP_NUM_LEDS_MAX * 30)
 // going over 1000 leds will drop fps too low for the effects to look good
 
-#define PIN_LED_STRIP               0
+#define PIN_LED_STRIP               13
 #define LED_STRIP_TYPE              WS2815
 #define LED_STRIP_COLOR_ORDER       RGB
 #define LED_STRIP_NUM_LEDS_MAX      600
