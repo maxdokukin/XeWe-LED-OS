ccfe292566c247292fb8e5457974112f0a6b617b
Max <tools.shiny.01@icloud.com>
2026-03-16 21:26:23 -0700

    testing timings

---
+++ static/data/tests/800khz_vs_spi_def_spi_speed/800khz_vs_spi_def_spi_speed.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a48d25704e9c568aa92251bc9387aaa0bb83d7eef0b6ea3fe10203679fa434d2
+size 20394
---
+++ static/data/tests/spi_clock_freq/spi_clock_freq.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2557a3cd8d7f54f67fa73793f62ed5915bb6ef02a31146d6755708c7c9c8fd1f
+size 20396
